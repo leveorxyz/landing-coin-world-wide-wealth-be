90a0e92363727f55bd71f1bede50c3738709c600
Jason Haque <zahinjason220434@gmail.com>
2022-08-25 20:27:27 +0600

    add:  basic auth functionality

--- index.ts
+++ index.ts
@@ -1,14 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import demoRoute from "./routes/demo";
+import authRoute from "./routes/auth.route";
 
 dotenv.config();
 
 const app = express();
 const port: number = parseInt(process.env.PORT || "8000");
-
+app.use(express.json());
 app.use("/", demoRoute);
-
+app.use("/auth", authRoute);
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running on port ${port}`);
 });
