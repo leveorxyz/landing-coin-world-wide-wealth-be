da54de4888ca3748752af94b2bc4c42e25247d76
sihan-tawsik <sihantawsik@gmail.com>
2022-09-05 19:51:05 +0600

    add: Stripe signature check on webhook

--- controllers/payment.controller.ts
+++ controllers/payment.controller.ts
@@ -1,15 +1,35 @@
 import dotenv from "dotenv";
 import stripe from "stripe";
 import { Request, Response } from "express";
+import { wrappedResponse } from "../utils/functions";
 
 dotenv.config();
 
 const stripeInstance = new stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: "2022-08-01",
 });
-const endpointSecret = "somekey";
+const endpointSecret = process.env.ENDPOINT_SECRET!;
 
 export const postWebhook = (req: Request, res: Response) => {
-  console.log(req.body);
-  res.send("ok");
+  const stripeSignature = req.headers["stripe-signature"];
+  let event;
+
+  try {
+    event = stripeInstance.webhooks.constructEvent(
+      req.body,
+      stripeSignature!,
+      endpointSecret
+    );
+  } catch (err: any) {
+    console.log(err.message);
+    return wrappedResponse(res, `Webhook error ${err.message}`, 400, null);
+  }
+
+  // Handle the event
+  if (event.type === "charge.succeeded") {
+    console.log("sucesssss");
+    return wrappedResponse(res, "payment success", 200, null);
+  }
+  console.log(event.type);
+  return res.status(200).send({});
 };
--- routes/payment.route.ts
+++ routes/payment.route.ts
@@ -6,3 +6,5 @@ import { methodNotAllowed } from "../utils/functions";
 const router = express.Router();
 
 router.route("/webhook").post(postWebhook).all(methodNotAllowed);
+
+export default router;
