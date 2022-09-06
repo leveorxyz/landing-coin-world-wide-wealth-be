8d393c441ee87e915f7d1e54aae1c0f26e5036a0
sihan-tawsik <sihantawsik@gmail.com>
2022-09-06 19:50:27 +0600

    add: Admin rent pay endpoint

--- controllers/rent.controller.ts
+++ controllers/rent.controller.ts
@@ -1,4 +1,7 @@
-import { findPropertyById } from "../datasource/property.datasource";
+import {
+  findPropertyById,
+  updatePropertyDueDate,
+} from "../datasource/property.datasource";
 import { Request, Response } from "express";
 import { wrappedResponse } from "../utils/functions";
 
@@ -18,4 +21,29 @@ export const getRentDue = async (req: Request, res: Response) => {
   months = months < 0 ? 0 : months;
   const result = months * property.price;
   return wrappedResponse(res, "Due rent calculated successfully", 200, result);
+};
+
+export const payRentByAdmin = async (req: Request, res: Response) => {
+  const property = await findPropertyById(req.body.id || "");
+  if (!property) {
+    return wrappedResponse(res, "Property not found", 404, null);
+  }
+
+  let currentDate = new Date();
+  let nextDate =
+    currentDate.getUTCMonth() === 11
+      ? new Date(
+          currentDate.getUTCFullYear(),
+          0,
+          1,
+          Math.round(-currentDate.getTimezoneOffset() / 60)
+        )
+      : new Date(
+          currentDate.getUTCFullYear(),
+          currentDate.getUTCMonth() + 1,
+          1,
+          Math.round(-currentDate.getTimezoneOffset() / 60)
+        );
+  await updatePropertyDueDate(property.id, nextDate.toISOString());
+  return wrappedResponse(res, "Updated successfuly", 200, null);
 };
--- routes/rent.route.ts
+++ routes/rent.route.ts
@@ -1,10 +1,12 @@
 import express from "express";
 
-import { getRentDue } from "../controllers/rent.controller";
+import { getRentDue, payRentByAdmin } from "../controllers/rent.controller";
 import { methodNotAllowed } from "../utils/functions";
+import { authMiddleware } from "../utils/middlewares";
 
 const router = express.Router();
 
 router.route("/due").get(getRentDue).all(methodNotAllowed);
+router.route("/admin-pay").post(payRentByAdmin).all(methodNotAllowed);
 
 export default router;
