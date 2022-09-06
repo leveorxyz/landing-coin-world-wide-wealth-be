24da0e8ab023cbb49e23cb9ced45cf9e9e30714a
Jason Haque <zahinjason220434@gmail.com>
2022-09-06 12:29:29 +0600

    add: create payment, find by id datasource functions

---
+++ datasource/payment.datasource.ts
@@ -0,0 +1,44 @@
+import { PrismaClient, TransactionInfo } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const createTransaction = async (
+  publicAddress: string,
+  amount: number,
+  pricePaid: number,
+  type: string
+) => {
+  await prisma.$connect();
+
+  return prisma.transactionInfo
+    .create({
+      data: {
+        publicAddress: publicAddress,
+        amount: amount,
+        pricePaid: pricePaid,
+        type: type,
+      },
+    })
+    .then(async (transaction: TransactionInfo | null) => {
+      await prisma.$disconnect();
+      return transaction;
+    });
+};
+
+export const findTransactionById = async (id: string) => {
+  await prisma.$connect();
+  return prisma.transactionInfo
+    .findUnique({
+      where: {
+        transactionId: id,
+      },
+    })
+    .then(async (transaction: TransactionInfo | null) => {
+      await prisma.$disconnect();
+      return transaction;
+    })
+    .catch(async (e: Error) => {
+      await prisma.$disconnect();
+      throw e;
+    });
+};
