0d98ab96056cd7b8ed9e10dbff4c68b3adfa42d0
Jason Haque <zahinjason220434@gmail.com>
2022-09-08 23:40:09 +0600

    add: create payment intent endpoint

--- controllers/payment.controller.ts
+++ controllers/payment.controller.ts
@@ -21,6 +21,24 @@ const stripeInstance = new stripe(process.env.STRIPE_SECRET_KEY!, {
 });
 const endpointSecret = process.env.ENDPOINT_SECRET!;
 
+export const createPaymentIntent = async (req: Request, res: Response) => {
+  const paymentIntent = await stripeInstance.paymentIntents.create({
+    amount: req.body.amount,
+    currency: "usd",
+    payment_method_types: ["card"],
+    description: req.body.description,
+  });
+  console.log(paymentIntent);
+  res.send(
+    wrappedResponse(
+      res,
+      "Payment intent created",
+      200,
+      paymentIntent.client_secret
+    )
+  );
+};
+
 export const postWebhook = async (req: Request, res: Response) => {
   const stripeSignature = req.headers["stripe-signature"];
   let event;
