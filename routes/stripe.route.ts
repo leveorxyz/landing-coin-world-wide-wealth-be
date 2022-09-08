cbcc42dba8e444791f7b4ae778241deebe38fc46
Jason Haque <zahinjason220434@gmail.com>
2022-09-09 01:45:06 +0600

    add: endpoint for rent payment intent

--- routes/stripe.route.ts
+++ routes/stripe.route.ts
@@ -1,10 +1,17 @@
 import express from "express";
 
-import { createPaymentIntent } from "../controllers/payment.controller";
+import {
+  createPaymentIntent,
+  createRentPaymentIntent,
+} from "../controllers/payment.controller";
 import { methodNotAllowed } from "../utils/functions";
 
 const router = express.Router();
 
 router.route("/intent").post(createPaymentIntent).all(methodNotAllowed);
+router
+  .route("/rent-intent")
+  .post(createRentPaymentIntent)
+  .all(methodNotAllowed);
 
 export default router;
