add90e089eeea404e4a7b5265fb3848b846755e1
sihan-tawsik <sihantawsik@gmail.com>
2022-09-14 12:28:53 +0600

    add: Timestamp calculation for cron

--- cronjobs/disbursement.ts
+++ cronjobs/disbursement.ts
@@ -1,6 +1,14 @@
 import { CronJob } from "cron";
 
-const fundDisburseCron = new CronJob("0 0 0 * * *", () => {
+const fundDisburseCron = new CronJob("0 0 0 7 * *", () => {
+  const currentDate = new Date();
+  const timeStamp =
+    new Date(
+      currentDate.getUTCFullYear(),
+      currentDate.getUTCMonth(),
+      1,
+      Math.round(-currentDate.getTimezoneOffset() / 60)
+    ).getTime() / 1000;
   console.log("disbursement done");
 });
 
